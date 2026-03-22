e772a9ef4aad4753f448b4efc8b3cfb61ee52cd9
Sophia Pérez <sophi3324.sp@gmail.com>
2026-03-21 19:52:45 -0600

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -11,7 +11,6 @@ import JoinClub from './pages/JoinClub/JoinClub';
 import Gallery from './pages/Gallery/Gallery';
 import './styles/global.scss';
 import MerchPage from './pages/Merch/MerchPage';
-import ChatbotWidget from './components/ChatbotWidget.tsx';
 
 
 const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY!);
