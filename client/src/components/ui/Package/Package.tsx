e8f0eaeecbe6bf92dc0e81771a5b28fa051434a5
Sophia Pérez <sophi3324.sp@gmail.com>
2026-03-23 18:22:23 -0600

    Update Package.tsx

--- client/src/components/ui/Package/Package.tsx
+++ client/src/components/ui/Package/Package.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useRef, useState } from 'react';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import { useTranslation } from 'react-i18next';
-import CheckoutModal from './Checkout/CheckoutModal'; 
+import CheckoutModal from '../../Checkout/CheckoutModal'; 
 import './Package.scss';
 
 gsap.registerPlugin(ScrollTrigger);
